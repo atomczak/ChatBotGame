292091d7b790bb87f3d1b28c30b7aad1de2ea1e7
atom3 <artomczak@gmail.com>
2018-12-28 18:16:20 +0100

    Add files via upload

---
+++ code/tokens.py
@@ -0,0 +1,8 @@
+# Tokens below should match the ones stated on Facebook Messenger Developers website:
+app_secret = "d32b0aa40ca9a22d7a20df18c52c2a63"
+client = "54f50814619f7782c85d0f982769252d"
+access = 'EAAIuhhHJXv4BAKA0ZCZANOc1P6TwIugF0IgHvcd51I54fnRJ0O2WD9IomqOnmzqeKH8BS6QbI0SZAjPPFyVulLKVqqPrgnpCyHiCcfM7PP97UOERvad7QPTzMTol84Qtcyggylgr3A6SYDJrXFyE9GvgaDl4CWyHyLgJ71GSAZDZD'
+verification = 'Q6ZskF4csJyvAEMnF9ua'   #haslo podane na fb dev w sekcji webhooks
+bot_id = '1368143226655403'
+artur_id = '2231584683532589'
+kuba_id = '2410700988971224'
